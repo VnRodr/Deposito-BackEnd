bd0b70671750e831792cfecea64c2fff33d88ceb
VnRodr <viniciur@protonmail.com>
2025-12-03 18:47:48 -0300

    Employee Controller e Service

--- src/main/java/site/api/api_teste/controllers/EmployeeController.java
+++ src/main/java/site/api/api_teste/controllers/EmployeeController.java
@@ -3,6 +3,7 @@ package site.api.api_teste.controllers;
 import java.net.URI;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,10 +16,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import site.api.api_teste.entities.Employee;
+import site.api.api_teste.services.EmployeeService;
 
 @RequestMapping("/api")
 @RestController
 public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
 
 @GetMapping("/employees")
     public ResponseEntity<List<Employee>> showAllemployees() 
---
+++ src/main/java/site/api/api_teste/services/EmployeeService.java
@@ -0,0 +1,46 @@
+package site.api.api_teste.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import site.api.api_teste.entities.Employee;
+import site.api.api_teste.reposities.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public List<Employee> findAll() {
+        List<Employee> EmployeeEntityList = employeeRepository.findAll();
+
+        return new ArrayList<>(EmployeeEntityList);
+    }
+
+    public Employee findById(String id)
+    {
+        return employeeRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee not found"));
+    }
+
+    public void saveEntity(Employee entity)
+    {
+        employeeRepository.save(entity);
+    }
+
+    public void changeEntity (Employee newEntity)
+    {
+        employeeRepository.save(newEntity);
+    }
+
+    public void deleteEntity (String id)
+    {
+        employeeRepository.deleteById(id);
+    }
+}
