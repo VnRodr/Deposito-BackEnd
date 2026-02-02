d5332b7fdb4eb72cfffd0e64e904225ec62ca511
VnRodr <viniciur@protonmail.com>
2026-02-02 20:29:21 -0300

    Mudança no GET endpoint para eliminação de ambiguidade

--- src/main/java/site/api/api_teste/controllers/EmployeeController.java
+++ src/main/java/site/api/api_teste/controllers/EmployeeController.java
@@ -2,7 +2,6 @@ package site.api.api_teste.controllers;
 
 import java.net.URI;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import site.api.api_teste.entities.Employee;
 import site.api.api_teste.enums.HttpsStatusCodes;
 import site.api.api_teste.services.EmployeeService;
@@ -21,36 +19,35 @@ import site.api.api_teste.services.EmployeeService;
 @RequestMapping("/api")
 @RestController
 public class EmployeeController {
-    @Autowired
-    private EmployeeService employeeService;
+  @Autowired private EmployeeService employeeService;
 
-    @GetMapping("/employees")
-    public ResponseEntity<List<Employee>> showAllemployees() {
-        return ResponseEntity.ok(employeeService.findAll());
-    }
+  @GetMapping("/employees")
+  public ResponseEntity<List<Employee>> showAllemployees() {
+    return ResponseEntity.ok(employeeService.findAll());
+  }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Employee> findById(@PathVariable String id) {
-        Employee employee = employeeService.findById(id);
-        return ResponseEntity.ok(employee);
-    }
+  @GetMapping("/employee/{id}")
+  public ResponseEntity<Employee> findById(@PathVariable String id) {
+    Employee employee = employeeService.findById(id);
+    return ResponseEntity.ok(employee);
+  }
 
-    @PostMapping("/create-employee")
-    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
-        employeeService.saveEntity(employee);
-        URI location = URI.create("/api/create-employee" + employee.getId());
-        return ResponseEntity.created(location).body(employee);
-    }
+  @PostMapping("/create-employee")
+  public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+    employeeService.saveEntity(employee);
+    URI location = URI.create("/api/create-employee" + employee.getId());
+    return ResponseEntity.created(location).body(employee);
+  }
 
-    @PutMapping("/update-employee")
-    public ResponseEntity<HttpsStatusCodes> atualizarEntidade(@RequestBody Employee employee) {
-        employeeService.changeEntity(employee);
-        return ResponseEntity.ok().build();
-    }
+  @PutMapping("/update-employee")
+  public ResponseEntity<HttpsStatusCodes> atualizarEntidade(@RequestBody Employee employee) {
+    employeeService.changeEntity(employee);
+    return ResponseEntity.ok().build();
+  }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<HttpsStatusCodes> deletarEntidade(@PathVariable String id) {
-        employeeService.deleteEntity(id);
-        return ResponseEntity.ok().build();
-    }
+  @DeleteMapping("/delete/{id}")
+  public ResponseEntity<HttpsStatusCodes> deletarEntidade(@PathVariable String id) {
+    employeeService.deleteEntity(id);
+    return ResponseEntity.ok().build();
+  }
 }
--- src/main/java/site/api/api_teste/controllers/StockGroupController.java
+++ src/main/java/site/api/api_teste/controllers/StockGroupController.java
@@ -2,7 +2,6 @@ package site.api.api_teste.controllers;
 
 import java.net.URI;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,43 +11,41 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import site.api.api_teste.entities.StockGroup;
 import site.api.api_teste.enums.HttpsStatusCodes;
 import site.api.api_teste.services.StockGroupService;
 
 @RestController
 public class StockGroupController {
-    @Autowired
-    private StockGroupService stockGroupService;
+  @Autowired private StockGroupService stockGroupService;
 
-    @GetMapping("/stockGroups")
-    public ResponseEntity<List<StockGroup>> showAllStockGroups() {
-        return ResponseEntity.ok(stockGroupService.findAll());
-    }
+  @GetMapping("/stockGroups")
+  public ResponseEntity<List<StockGroup>> showAllStockGroups() {
+    return ResponseEntity.ok(stockGroupService.findAll());
+  }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<StockGroup> findById(@PathVariable String id) {
-        StockGroup stockGroup = stockGroupService.findById(id);
-        return ResponseEntity.ok(stockGroup);
-    }
+  @GetMapping("/stockGroup/{id}")
+  public ResponseEntity<StockGroup> findById(@PathVariable String id) {
+    StockGroup stockGroup = stockGroupService.findById(id);
+    return ResponseEntity.ok(stockGroup);
+  }
 
-    @PostMapping("/create-stockGroup")
-    public ResponseEntity<StockGroup> createStockGroup(@RequestBody StockGroup stockGroup) {
-        stockGroupService.saveEntity(stockGroup);
-        URI location = URI.create("/api/create-stockGroup" + stockGroup.getId());
-        return ResponseEntity.created(location).body(stockGroup);
-    }
+  @PostMapping("/create-stockGroup")
+  public ResponseEntity<StockGroup> createStockGroup(@RequestBody StockGroup stockGroup) {
+    stockGroupService.saveEntity(stockGroup);
+    URI location = URI.create("/api/create-stockGroup" + stockGroup.getId());
+    return ResponseEntity.created(location).body(stockGroup);
+  }
 
-    @PutMapping("/update-stockGroup")
-    public ResponseEntity<HttpsStatusCodes> atualizarEntidade(@RequestBody StockGroup stockGroup) {
-        stockGroupService.changeEntity(stockGroup);
-        return ResponseEntity.ok().build();
-    }
+  @PutMapping("/update-stockGroup")
+  public ResponseEntity<HttpsStatusCodes> atualizarEntidade(@RequestBody StockGroup stockGroup) {
+    stockGroupService.changeEntity(stockGroup);
+    return ResponseEntity.ok().build();
+  }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<HttpsStatusCodes> deletarEntidade(@PathVariable String id) {
-        stockGroupService.deleteEntity(id);
-        return ResponseEntity.ok().build();
-    }
+  @DeleteMapping("/delete/{id}")
+  public ResponseEntity<HttpsStatusCodes> deletarEntidade(@PathVariable String id) {
+    stockGroupService.deleteEntity(id);
+    return ResponseEntity.ok().build();
+  }
 }
